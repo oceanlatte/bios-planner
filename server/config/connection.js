54511e63da6772374af8c0d8e564d69d7978bbb5
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-10-24 16:22:09 -0500

    added server and also test schemas to make sure apollo sandbox can run

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/biosplanner', {
-    useNewURLParser: true,
+    useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
