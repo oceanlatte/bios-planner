37d1450097f008979baf562ee91090d1408a24f4
ocean <osciris.veliz@gmail.com>
2022-11-01 14:37:03 -0500

    merge

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,26 +1,13 @@
-<<<<<<< HEAD
-import { useState } from "react";
-=======
 import React, { useState } from "react";
->>>>>>> feature/add-buttons
 // import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Todo from "../components/Todo/Todo";
 import Budget from "../components/Budget/Budget";
-<<<<<<< HEAD
-import AddTodo from "../components/AddTodo/AddTodo";
-import AddBudget from "../components/AddBudget/AddBudget"
-
-
-const Profile = () => {
-  const [showComponent, setShowComponent] = useState(true);
-=======
 import AddTodos from "./AddTodo";
 import AddBudgetTotal from "./AddBudget"
 
 const Profile = () => {
   const [showComponent, setShowComponent] = useState(true);
 
->>>>>>> feature/add-buttons
   return (
     <div>
     {showComponent ? <AddTodo /> : ""}
