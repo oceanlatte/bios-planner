8eaff01ad6599a9b9ed6114219dd239ecb0f7714
ocean <osciris.veliz@gmail.com>
2022-11-02 16:01:35 -0500

    add todo list checkboxes and styling; pass filtered daily task props to todo componenet

--- client/src/components/Todo/Todo.js
+++ client/src/components/Todo/Todo.js
@@ -1,35 +1,30 @@
 import React from "react";
-import { useQuery } from "@apollo/client";
-import { QUERY_TODOS } from "../../utils/queries";
 
-const Todo = () => {
-  const { data: todoData } = useQuery(QUERY_TODOS);
+const Todo = (tName) => {
+  // console.log('PROPS', tName);
+  // console.log('PROPS . todoName:', tName.todoNames);
 
-  const todoList = todoData ? todoData.todos : [];
-
-  let daily = [];
   let names = [];
 
-  if (todoList.length) {
-    const dailyList = Object.getOwnPropertyNames(todoList).forEach((val) => {
-      if (todoList[val].recurrence === "daily") {
-        daily.push(todoList[val].todoName);
-      }
-    });
-    console.log("DAILY todos ONLY", daily);
-
-    const newList = Object.getOwnPropertyNames(todoList).forEach((val) => {
-      names.push(todoList[val].todoName);
+  if (tName) {
+    const newList = Object.getOwnPropertyNames(tName).forEach((val) => {
+      names.push(tName[val]);
     });
   }
-  console.log(names);
+
+  console.log('NAMES ONLY, todo Params filter', names)
 
   return (
     <div className="card">
       <div>
         <h3 className="title">To Do List</h3>
-        <ul>
-          {todoList.length && names.map((todo, i) => <li key={i}>{todo}</li>)}
+        <ul className="todo-list-container">
+          {tName && names.map((todo, i) => 
+          <li key={i} className="todo-list">
+            <input type="checkbox" className="checkbox" />
+            <label for={i} name={todo} value={todo}>{todo}</label>
+          </li>
+          )}
         </ul>
       </div>
     </div>
--- client/src/pages/Daily.js
+++ client/src/pages/Daily.js
@@ -1,10 +1,30 @@
 import Todo from "../components/Todo/Todo";
 import Budget from "../components/Budget/Budget";
 
-import { useParams } from 'react-router-dom';
+import { useQuery } from "@apollo/client";
+import { QUERY_TODOS } from "../utils/queries";
 
 function Daily() {
-  const { username: userParam } = useParams();
+  const { data: todoData } = useQuery(QUERY_TODOS);
+
+  const todoList = todoData ? todoData.todos : [];
+
+  let daily = [];
+  let names = [];
+
+  if (todoList.length) {
+    Object.getOwnPropertyNames(todoList).forEach((val) => {
+      if (todoList[val].recurrence === "daily") {
+        daily.push(todoList[val].todoName);
+      }
+    });
+    console.log("DAILY todos ONLY", daily);
+
+    // const newList = Object.getOwnPropertyNames(todoList).forEach((val) => {
+    //   names.push(todoList[val].todoName);
+    // });
+  }
+  // console.log(names);
   
   return (
     <div>
@@ -12,7 +32,8 @@ function Daily() {
     
       <div className="container">
         <Budget />
-        <Todo />
+        <Todo 
+        todoNames={daily}/>
       </div>
 
     </div>
