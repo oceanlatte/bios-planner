4d9c9ad0101b26700a65efe1b9d75bea63a2bf2d
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-11-02 14:39:26 -0500

    merge conflict

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,10 +1,44 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_USER, QUERY_USERS } from '../utils/queries';
 
 import Todo from "../components/Todo/Todo";
 import Budget from "../components/Budget/Budget";
+import Auth from '../utils/auth'
 
-const Profile = () => {
+const Profile = (props) => {
+  const { username: userParam } = useParams();
+
+  const { loading, data} = useQuery(QUERY_USER);
+
+  console.log('from Profile user PARAMS:', userParam);
+ 
+  console.log('from Profile CURRENT user:', data);
+
+  const currentUser = data? data.currentUser.username : {};
+  console.log('Current User log:', currentUser);
+  
+  // navigate to personal profile page if username is yours
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Navigate to="/profile" />;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!Auth.loggedIn()) {
+  
+    return (
+      <h4>
+        You need to be logged in to see this. Use the navigation links above to
+        sign up or log in!
+      </h4>
+    );
+  }
+
   return (
     <div className="profile-container">
       <div className="add-btn-list"> 
